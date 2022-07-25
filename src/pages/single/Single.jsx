009ff9f5a00d9c1c45f1ee052a3bc1641b3f96e6
PaulGuillen <paulguillen190@gmail.com>
2022-07-25 14:14:24 -0500

    Editing Single page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -8,6 +8,20 @@ const Single = () => {
             <Sidebar/>
             <div className="singleContainer">
                 <Navbar/>
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Editar</div>
+                        <h1 className="title">Informacion</h1>
+                        <div className="item">
+                            <img src="https://iteragrow.com/wp-content/uploads/2018/04/buyer-persona-e1545248524290.jpg"
+                            alt=""
+                            className="itemImg" />
+                             details
+                        </div>
+                    </div>
+                    <div className="right"></div>
+                </div>
+                <div className="bottom"></div>
             </div>
         </div>
     )

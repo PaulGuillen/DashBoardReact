601bf355fa08b31dca6cbce9fdcfd64fdf5cd3c7
PaulGuillen <paulguillen190@gmail.com>
2022-07-26 10:54:10 -0500

    Creating endpoint new user

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,11 +1,28 @@
-import "./new.scss"
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const New = () => {
-    return(
-        <div className="new">
-            New
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Nuevo Usario</h1>
         </div>
-    )
-}
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Pierre-Person.jpg"
+              alt=""
+            />
+          </div>
+          <div className="right">Right</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default New
+export default New;
